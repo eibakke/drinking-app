eddfd7aab0ec015b75fcfe5c2d317ec50a10475e
Eivind <eivind.m.bakke@gmail.com>
2014-03-03 16:29:34 -0600

    Started up the user class

---
+++ HEALUser.h
@@ -0,0 +1,18 @@
+//
+//  HEALUser.h
+//  drinkingApp
+//
+//  Created by Eivind Bakke on 3/3/14.
+//  Copyright (c) 2014 Halealei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HEALUser : NSObject
+
+@property(strong, nonatomic) NSString *userSex;
+@property(strong,nonatomic) NSNumber *userWeight;
+@property(strong,nonatomic) NSString *userName;
+
+
+@end
---
+++ HEALUser.m
@@ -0,0 +1,13 @@
+//
+//  HEALUser.m
+//  drinkingApp
+//
+//  Created by Eivind Bakke on 3/3/14.
+//  Copyright (c) 2014 Halealei. All rights reserved.
+//
+
+#import "HEALUser.h"
+
+@implementation HEALUser
+
+@end
