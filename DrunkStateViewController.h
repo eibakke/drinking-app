cb16c201e03aaa993c08169facb290fcf33a6e8a
haroon477 <mbokhari@macalester.edu>
2014-03-03 12:09:41 -0600

    added drunk state view and basic implementation of this

---
+++ DrunkStateViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrunkStateViewController.h
+//  drinkingApp
+//
+//  Created by Haroon Bokhary on 3/3/14.
+//  Copyright (c) 2014 Halealei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrunkStateViewController : UIViewController
+
+@end
---
+++ DrunkStateViewController.m
@@ -0,0 +1,38 @@
+//
+//  DrunkStateViewController.m
+//  drinkingApp
+//
+//  Created by Haroon Bokhary on 3/3/14.
+//  Copyright (c) 2014 Halealei. All rights reserved.
+//
+
+#import "DrunkStateViewController.h"
+
+@interface DrunkStateViewController ()
+
+@end
+
+@implementation DrunkStateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
