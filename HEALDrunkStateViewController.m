8f8b2fd40d906b270463d7713b4c34519113e7c4
ABASSEN <abassen@macalester.edu>
2014-04-11 08:29:23 -0500

    changed state screens so they look less like buttons. still need info in them

--- HEALDrunkStateViewController.m
+++ HEALDrunkStateViewController.m
@@ -19,7 +19,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -27,7 +26,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -39,62 +37,12 @@
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:image];
 
-    /*if(self.user.BAC < 0.02)
-    {
-        UIGraphicsBeginImageContext(self.view.frame.size);
-        [[UIImage imageNamed:@"SoberState.png"] drawInRect:self.view.bounds];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-        
-        
-        
-    } else if(0.02 < self.user.BAC && self.user.BAC < 0.06)
-    {
-        UIGraphicsBeginImageContext(self.view.frame.size);
-        [[UIImage imageNamed:@"TipsyState.png"] drawInRect:self.view.bounds];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-        
-        
-        
-    } else if(0.06 < self.user.BAC && self.user.BAC < 0.2)
-    {
-        UIGraphicsBeginImageContext(self.view.frame.size);
-        [[UIImage imageNamed:@"DrunkState.png"] drawInRect:self.view.bounds];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-        
-    } else if(0.2 < self.user.BAC && self.drinkStepper.value != 100 )
-    {
-        UIGraphicsBeginImageContext(self.view.frame.size);
-        [[UIImage imageNamed:@"DangerState.png"] drawInRect:self.view.bounds];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-        
-    }if(3.4 < self.user.BAC)
-    {
-        UIGraphicsBeginImageContext(self.view.frame.size);
-        [[UIImage imageNamed:@"DeadState.png"] drawInRect:self.view.bounds];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-    }*/
-}
+    }
 
     
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
