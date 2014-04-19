2286ceeebec254d9a6dffb7011d39d38d6db8759
ABASSEN <abassen@macalester.edu>
2014-04-19 16:03:40 -0500

    added launch image/splash screen. transition to our loading screen in a little wonky

--- drinkingApp/HEALAppDelegate.m
+++ drinkingApp/HEALAppDelegate.m
@@ -19,7 +19,9 @@
     
     [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"sampleNavBar1.png"] forBarMetrics:UIBarMetricsDefault];
     
+    
     //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:NO];
+    
     
     return YES;
 }
